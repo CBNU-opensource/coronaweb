2ee09e3493685355aa7082bc21d377131e026b8c
Lee Jin Won <48896377+dsjinwongo@users.noreply.github.com>
2020-06-19 19:36:39 +0900

    진짜 마지막!

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import krdaily, Country, World_daily
+from .models import krdaily, Country, World_daily, kr_region
 
 admin.site.register(krdaily)
 admin.site.register(Country)
 admin.site.register(World_daily)
+admin.site.register(kr_region)
