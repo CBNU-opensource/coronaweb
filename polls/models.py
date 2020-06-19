0394f9abf893ea3f8ff0672c6696b4ad7506321c
Lee Jin Won <48896377+dsjinwongo@users.noreply.github.com>
2020-06-19 16:21:53 +0900

    마지막!

--- polls/models.py
+++ polls/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class krdaily(models.Model):
-    kr_date=models.CharField(max_length=200)
-    kr_confirmed=models.CharField(max_length=200)
-    kr_death=models.CharField(max_length=200)
-    kr_released=models.CharField(max_length=200)
-    kr_candidate=models.CharField(max_length=200)
-    kr_negative=models.CharField(max_length=200)
+    kr_date=models.CharField(max_length=20)
+    kr_confirmed=models.TextField()
+    kr_death=models.CharField(max_length=20)
+    kr_released=models.CharField(max_length=20)
+    kr_candidate=models.CharField(max_length=20)
+    kr_negative=models.CharField(max_length=20)
 
     def __str__(self):
         return self.kr_date
-
 
 class Country(models.Model):
     name = models.CharField(max_length=15)
@@ -22,3 +19,25 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+class World_daily(models.Model):
+    world_today_date=models.CharField(max_length=20)
+    world_total_confirmed=models.CharField(max_length=20)
+    world_new_cases=models.CharField(max_length=20)
+    world_total_death=models.CharField(max_length=20)
+    world_new_death=models.CharField(max_length=20)
+    world_deaths_rate=models.CharField(max_length=20)
+    world_country=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.world_country
+
+class kr_region(models.Model):
+    kr_date=models.CharField(max_length=20)
+    kr_region=models.CharField(max_length=20)
+    kr_confirmed=models.CharField(max_length=20)
+    kr_death=models.CharField(max_length=20)
+    kr_released=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.kr_date
